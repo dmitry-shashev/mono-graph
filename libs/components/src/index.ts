f2f05a4bbd3c77f0fa8588b9657d5fdb4957ac3b
Dmitry Shashev <old.reaver@gmail.com>
2022-08-07 15:42:44 +0300

    Improve imports and exports

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -1,6 +1,7 @@
 export * from './lib/components/TopNavigation/TopNavigation'
-export * from './lib/components/ProfileForm/ProfileForm'
 export * from './lib/components/HourlyEnergyChart/HourlyEnergyChart'
 export * from './lib/components/PowerFlowDiagram/PowerFlowDiagram'
 
 export * from './lib/interfaces/form-props'
+
+export * from './lib/forms/ProfileForm/ProfileForm'
