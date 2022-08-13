0e81afa4bd5a14ab7d105fc3b0b7eae6c797727b
Dmitry Shashev <old.reaver@gmail.com>
2022-08-13 12:09:39 +0300

    Forms tests and refactoring

---
+++ libs/components/src/lib/forms/ProfileForm/ProfileForm.test.tsx
@@ -0,0 +1,53 @@
+import { render } from '@testing-library/react'
+import { ProfileForm } from './ProfileForm'
+import { clickByAriaLabel, typeInInputByAriaLabel } from '../../test/test-utils'
+import { getTestUser } from '../../test/test-data'
+
+describe('ProfileForm', () => {
+  it('component', async () => {
+    const onSubmit = jest.fn()
+    const testUser = getTestUser()
+    const newUserLastName = 'Rain'
+
+    render(
+      <ProfileForm
+        defaultValues={testUser}
+        onSubmit={(data) => onSubmit(data)}
+      />
+    )
+
+    // update the last name
+    await typeInInputByAriaLabel('Last Name', newUserLastName)
+
+    // submit and check the form
+    await clickByAriaLabel('Submit the form')
+    expect(onSubmit).toBeCalledWith({
+      ...testUser,
+      lastName: newUserLastName,
+    })
+  })
+
+  it('component with errors', async () => {
+    const onSubmit = jest.fn()
+    const testUser = getTestUser()
+
+    render(
+      <ProfileForm
+        defaultValues={testUser}
+        onSubmit={(data) => onSubmit(data)}
+      />
+    )
+
+    await typeInInputByAriaLabel('Last Name', '')
+    await clickByAriaLabel('Submit the form')
+    expect(onSubmit).toBeCalledTimes(0)
+
+    await typeInInputByAriaLabel('Last Name', 'Rain$')
+    await clickByAriaLabel('Submit the form')
+    expect(onSubmit).toBeCalledTimes(0)
+
+    await typeInInputByAriaLabel('Last Name', 'Mike')
+    await clickByAriaLabel('Submit the form')
+    expect(onSubmit).toBeCalledTimes(1)
+  })
+})
---
+++ libs/components/src/lib/forms/form-error-messages.ts
@@ -0,0 +1,4 @@
+export const FIELD_REQUIRED_MESSAGE = 'The field is required'
+
+export const NOT_ALLOWED_SYMBOLS_MESSAGE =
+  'Next symbols are not allowed - ,=´`\'#&%§"!°_:;\\+?.*^$(){}|[]/'
