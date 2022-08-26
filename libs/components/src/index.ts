cd2f669681c5806034340d661faa13f9711643ce
Dmitry Shashev <old.reaver@gmail.com>
2022-08-26 12:47:48 +0300

    Extra modals

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -7,3 +7,5 @@ export * from './lib/interfaces/form-props'
 export * from './lib/forms/ProfileForm/ProfileForm'
 
 export * from './lib/modals/InfoModal'
+export * from './lib/modals/ConfirmationModal'
+export * from './lib/modals/UserModal'
