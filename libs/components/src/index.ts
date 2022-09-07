b8c957c72ceddae4bf1bc1af77a5dfa019d09a74
Dmitry Shashev <36708836+dmitry-shashev@users.noreply.github.com>
2022-09-07 22:18:53 +0300

    Move modals (#37)
    * Move modals in a separate module
    
    * Move modals

--- libs/components/src/index.ts
+++ libs/components/src/index.ts
@@ -6,7 +6,3 @@ export * from './lib/components/Tabs/Tabs'
 export * from './lib/interfaces/form-props'
 
 export * from './lib/forms/ProfileForm/ProfileForm'
-
-export * from './lib/modals/InfoModal/InfoModal'
-export * from './lib/modals/ConfirmationModal/ConfirmationModal'
-export * from './lib/modals/UserModal/UserModal'
---
+++ libs/modals/src/index.ts
@@ -0,0 +1,3 @@
+export * from './lib/modals/InfoModal/InfoModal'
+export * from './lib/modals/UserModal/UserModal'
+export * from './lib/modals/ConfirmationModal/ConfirmationModal'
