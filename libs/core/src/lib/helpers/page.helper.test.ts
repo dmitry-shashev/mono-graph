91239f6199f9c32a86fda1357c932338bd6cf253
Dmitry Shashev <old.reaver@gmail.com>
2022-09-27 20:29:41 +0300

    Small helpers

--- libs/core/src/lib/helpers/page.helper.test.ts
+++ libs/core/src/lib/helpers/page.helper.test.ts
@@ -3,7 +3,7 @@ import { PageHelper } from './page.helper'
 describe('page.helper', () => {
   it('buildUrl', async () => {
     expect(PageHelper.buildUrl('/some/one', '13')).toBe('/some/one')
-    expect(PageHelper.buildUrl('/some/[id]/12', '13')).toBe('/some/MTM=/12')
+    expect(PageHelper.buildUrl('/some/[id]/12', '13')).toBe('/some/MTM%3D/12')
     expect(PageHelper.buildUrl('/test/[id]')).toBe('/test/[id]')
     expect(PageHelper.buildUrl('/one')).toBe('/one')
     expect(PageHelper.buildUrl('')).toBe('')
