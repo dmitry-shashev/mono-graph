897bca0d03ebf0e72e597fe34abd8802b046b44e
Dmitry Shashev <36708836+dmitry-shashev@users.noreply.github.com>
2022-12-22 20:01:45 +0200

    Arr helper - unique (#86)

--- libs/core/src/lib/helpers/arr.helper.test.ts
+++ libs/core/src/lib/helpers/arr.helper.test.ts
@@ -1,5 +1,10 @@
 import { ArrHelper } from './arr.helper'
 
+interface TestElem {
+  id: number
+  no: string
+}
+
 describe('arr.helper', () => {
   it('sum', async () => {
     expect(ArrHelper.sum([false, null, 2, undefined, 3])).toBe(5)
@@ -11,5 +16,58 @@ describe('arr.helper', () => {
     expect(ArrHelper.allValuesEmpty([])).toBe(true)
     expect(ArrHelper.allValuesEmpty([false, undefined, 0])).toBe(true)
     expect(ArrHelper.allValuesEmpty([false, undefined, 1])).toBe(false)
+  })
+
+  it('addUnique', () => {
+    const getData = (): Array<number> => [3, 7, 8]
+    const data = getData()
+
+    expect(ArrHelper.addUnique(3, getData())).toEqual(getData())
+    expect(ArrHelper.addUnique(12, getData())).toEqual([3, 7, 8, 12])
+
+    expect(data).toEqual(getData())
+  })
+
+  it('addUnique - custom compare fn', () => {
+    const getData = (): ReadonlyArray<TestElem> => [
+      {
+        id: 1,
+        no: '2a',
+      },
+      {
+        id: 2,
+        no: '1a',
+      },
+    ]
+    const data = getData()
+
+    expect(
+      ArrHelper.addUnique(
+        {
+          id: 2,
+          no: 'lopata',
+        },
+        data,
+        (a, b) => a.id === b.id
+      )
+    ).toEqual(getData())
+    expect(
+      ArrHelper.addUnique(
+        {
+          id: 2,
+          no: 'lopata',
+        },
+        data,
+        (a, b) => a.no === b.no
+      )
+    ).toEqual([
+      ...data,
+      {
+        id: 2,
+        no: 'lopata',
+      },
+    ])
+
+    expect(data).toEqual(getData())
   })
 })
