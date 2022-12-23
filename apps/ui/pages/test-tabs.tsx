08e497afed1faa32ee3ec43b32602d9d5aa60ad8
Dmitry <old.reaver@gmail.com>
2022-12-23 21:09:18 +0200

    Fix e2e

--- apps/ui/pages/test-tabs.tsx
+++ apps/ui/pages/test-tabs.tsx
@@ -1,12 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { LayoutKind, Page } from '@mono-graph/core'
 import PagePath from '../lib/constants/page-path'
 import { Tabs } from '@mono-graph/components'
 
 const TestTabsPage: Page = () => {
+  const [tab, setTab] = useState<number>(1)
   return (
     <div>
-      <Tabs titles={['Tab 1', 'Tab 2', 'Tab 3']} currentTab={1}>
+      <Tabs
+        titles={['Tab 1', 'Tab 2', 'Tab 3']}
+        currentTab={tab}
+        onTabChange={setTab}
+      >
         <div>Some tab 1</div>
         <div>Some tab 2</div>
         <div>Some tab 3</div>
