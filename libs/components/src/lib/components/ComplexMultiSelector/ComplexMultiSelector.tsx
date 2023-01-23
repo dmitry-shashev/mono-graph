7a0d79addf070f0e4aeb05b0a1c5f828dcd61c2a
Dmitry <old.reaver@gmail.com>
2023-01-23 21:27:02 +0200

    Complex selector - start

---
+++ libs/components/src/lib/components/ComplexMultiSelector/ComplexMultiSelector.tsx
@@ -0,0 +1,115 @@
+import React, { FC, useState } from 'react'
+import styles from './ComplexMultiSelector.module.scss'
+import { StrHelper, Value, ValueHelper } from '@mono-graph/core'
+import clsx from 'clsx'
+
+interface Props {
+  possible: ReadonlyArray<Value>
+  selected: ReadonlyArray<Value>
+  onChange: (newSelected: Array<Value>) => void
+  topLabel: string
+  hideSelectAll?: boolean
+  singleMod?: boolean
+  showDetails?: boolean
+  onDetails?: () => void
+}
+
+export const ComplexMultiSelector: FC<Props> = ({
+  possible,
+  selected,
+  onChange,
+  topLabel,
+  singleMod,
+  hideSelectAll,
+  showDetails,
+  onDetails,
+}) => {
+  const [search, setSearch] = useState<string>('')
+
+  const onSelectAll = (newValue: boolean): void => {
+    const valueToSend = newValue ? [...possible] : []
+    onChange(valueToSend)
+  }
+
+  const onChangeInner = (currentElem: Value) => (): void => {
+    if (singleMod) {
+      onChange([currentElem])
+      return
+    }
+    const newSelected = selected.filter((v) => v.value !== currentElem.value)
+    if (newSelected.length === selected.length) {
+      newSelected.push(currentElem)
+    }
+    onChange(newSelected)
+  }
+
+  const hideCheckboxes = singleMod || hideSelectAll
+  const filteredPossible = StrHelper.applyFilterToCollection(
+    possible,
+    search,
+    ValueHelper.labelSelector
+  )
+  return (
+    <div
+      className={clsx(styles.wrap, {
+        [styles.singleMod ?? '']: singleMod,
+      })}
+    >
+      <div className={styles.titleWrap}>
+        <div className={styles.labelWrap}>
+          <input
+            hidden={hideCheckboxes}
+            type="checkbox"
+            checked={selected.length === possible.length}
+            onChange={(e) => onSelectAll(e.target.checked)}
+          />
+          <span className={styles.label}>{topLabel}</span>
+        </div>
+        <div className={styles.searchWrap}>
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            className={`defaultInput ${styles.filterInput}`}
+            type="text"
+            placeholder="Filter..."
+          />
+          {showDetails && (
+            <button
+              disabled={selected.length === 0}
+              onClick={onDetails}
+              className={styles.detailsBtn}
+              type="button"
+            />
+          )}
+        </div>
+      </div>
+
+      <div className={styles.contentWrap}>
+        {filteredPossible
+          .filter((v) => StrHelper.isSubstring(v.label ?? '', search))
+          .map((elem) => (
+            <div
+              style={{
+                color: elem.color ?? 'initial',
+              }}
+              onClick={onChangeInner(elem)}
+              className={clsx(styles.contentRow, {
+                [styles.contentRowSelected ?? '']: ValueHelper.includes(
+                  selected,
+                  elem
+                ),
+              })}
+            >
+              <input
+                hidden={hideCheckboxes}
+                checked={ValueHelper.includes(selected, elem)}
+                onChange={onChangeInner(elem)}
+                type="checkbox"
+              />
+              <div>{elem.label}</div>
+            </div>
+          ))}
+      </div>
+    </div>
+  )
+}
