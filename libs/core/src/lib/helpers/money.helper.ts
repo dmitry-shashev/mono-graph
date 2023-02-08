248cd2b97ef165a7d27b4dce68d23e6b754193da
Dmitry Shashev <36708836+dmitry-shashev@users.noreply.github.com>
2023-02-08 22:19:01 +0200

    Money helper small fix (#97)

--- libs/core/src/lib/helpers/money.helper.ts
+++ libs/core/src/lib/helpers/money.helper.ts
@@ -2,6 +2,7 @@ export abstract class MoneyHelper {
   public static format(value: number, currency = 'USD'): string {
     const formatCurrency = new Intl.NumberFormat(undefined, {
       style: 'currency',
+      currencyDisplay: 'narrowSymbol',
       currency,
     })
     return formatCurrency.format(value)
