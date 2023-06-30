ba4d2a62020ce9244533b74c7544fc456d839186
Dmitry <old.reaver@gmail.com>
2023-06-30 11:15:23 +0300

    Url with params

--- libs/core/src/lib/helpers/page.helper.ts
+++ libs/core/src/lib/helpers/page.helper.ts
@@ -12,4 +12,20 @@ export abstract class PageHelper {
     const idEnc = StrHelper.encodeURLParam(id)
     return `${url}?id=${idEnc}`
   }
+
+  public static buildUrlWithQuery(
+    url: string,
+    params: Record<string, string | number | null | undefined>
+  ): string {
+    const paramsFinal: Record<string, string> = {}
+    for (const key in params) {
+      const value = params[key]
+      if (value !== undefined && value !== null) {
+        paramsFinal[key] = String(value)
+      }
+    }
+    const query = new URLSearchParams(paramsFinal)
+
+    return `${url}?${query.toString()}`
+  }
 }
